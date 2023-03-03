0cc31390269c64503abc5282afbf8a4fde27c81c
bkraske <71412733+bkraske@users.noreply.github.com>
2023-03-03 16:31:23 -0700

    Add PBVI and other clean-up

--- test/mytest.jl
+++ test/mytest.jl
@@ -1,5 +1,5 @@
 using POMDPs, POMDPTools
-using SARSOP
+using PBVI
 using POMDPModels, RockSample
 
 rs = RockSamplePOMDP(5, 7)
@@ -8,10 +8,12 @@ cb = BabyPOMDP()
 tm = TMaze()
 mh = MiniHallway()
 
-solver = SARSOPSolver() 
+solver = PBVISolver(max_iter=10, verbose=true, witness_b=true) 
 t_pol = solve(solver,tiger)
 t_up = DiscreteUpdater(tiger)
 t_b0 = initialize_belief(t_up,initialstate(tiger))
 # t_pt = POMDPPolicyGraphs.policy_tree(tiger, t_up, t_pol, t_b0, 5)
 # t_pg = policy2fsc(tiger, t_up, t_pol, t_b0, 5)
 # t_pg_e = POMDPPolicyGraphs.GenandEvalPG(tiger, t_up, t_pol, t_b0, 5)
+
+
